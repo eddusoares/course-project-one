f4cd97d64d81ffe6cae5852fb0db427414b109e0
Eduardo Soares <eddu.soaresn@gmail.com>
2023-02-07 07:49:44 -0300

    Recipe item edited

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('Chicken Breast', 'Chicken breasts are a great way to make your own chicken.', 
+    'https://www.primaverakitchen.com/wp-content/uploads/2019/03/Garlic-Butter-Baked-Chicken-Breast-Primavera-Kitchen-2-800x1200.jpg'),
+  ];
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
